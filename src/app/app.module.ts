a478d21aebd590f72cd9059e5072f9c2a907ee64
Felipe Pereira <felipepereiracg@gmail.com>
2021-03-03 22:43:31 -0300

    Add rotas e pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { Error404Component } from './404/error404.component';
+import { NavBarComponent } from './navBar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,16 +8,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    NavBarComponent,
+    Error404Component,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+      path: '',
+      redirectTo: 'courses',
+      pathMatch:'full'
+    },
+    {
+      path: 'courses',
+      component: CourseListComponent
+    },
+    {
+      path:'**',
+      component:Error404Component
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
